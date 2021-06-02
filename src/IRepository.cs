58e74348599e5412364f6206f3767deff2d1f7da
Lucas Frois <lucas.frois@lambda3.com.br>
2021-06-02 00:11:08 -0300

    .

---
+++ src/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Select();
+        T Select<TKey>(TKey key);
+        IEnumerable<T> Select(Func<T, bool> expr);
+        void Insert(T entity);
+
+        void BulkInsert(IEnumerable<T> entities, bool rollback);
+        void Update(T entity);
+
+        void Delete<TKey>(TKey id);
+    }
+}
